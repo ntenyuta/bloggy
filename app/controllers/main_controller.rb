5ecf96f0211ec2f9adcf4cdd453fd6dd54844985
natalia tenyuta <nataliatenyuta@gmail.com>
2013-11-20 20:50:04 -0500

    initial creation

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,17 @@
+class MainController < ApplicationController
+  def about_us
+  	# Rails.logger.info "I am here, Natalia"
+  	#@founders = ["Richard Simms", "Betsy Houzer III"]
+	@company_name = "sdfsdf"
+  end
+
+  def privacy
+  end
+
+  def faq
+  end
+
+  def tos
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloggy::Application.config.secret_key_base = '72fbe0a0b04c003583dca7465d3441666ef7e38c937d3fee503100a7eb61c9fe18f03a785db2e008ce406973cd26faa7d6ddc4e4da631ffddb2f0ac86d97c9a1'
